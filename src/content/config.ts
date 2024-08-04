1aac91d7e45ac6bc119eb60f7ebc37ddc077ff36
fernando-paredes <ferparmur@gmail.com>
2024-08-04 03:26:28 +0100

    add prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,18 @@
-import {defineCollection, z} from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 const blogCollection = defineCollection({
-    type: 'content',
-    schema: ({image}) => z.object({
-        title: z.string(),
-        pubDate: z.date(),
-        author: z.string(),
-        coverImage: image().refine((img) => img.width >= 1080, {
-            message: "Cover image must be at least 1080 pixels wide!",
+    type: "content",
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            pubDate: z.date(),
+            author: z.string(),
+            coverImage: image().refine((img) => img.width >= 1080, {
+                message: "Cover image must be at least 1080 pixels wide!",
+            }),
+            coverImageAlt: z.string(),
+            tags: z.array(z.string()),
         }),
-        coverImageAlt: z.string(),
-        tags: z.array(z.string())
-    }),
 });
 
 export const collections = {
