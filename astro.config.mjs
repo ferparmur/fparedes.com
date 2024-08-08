882cdebcae745329dddf3633ada18b932788e516
fernando-paredes <ferparmur@gmail.com>
2024-08-08 18:30:09 +0100

    add html minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import { externalLink } from './src/externalLink.ts'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { externalLink } from "./src/externalLink.ts";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), sitemap()],
-    site: 'https://www.fparedes.com',
+    integrations: [mdx(), sitemap(), playformCompress()],
+    site: "https://www.fparedes.com",
     markdown: {
-        rehypePlugins: [[externalLink, { domain: 'fparedes.com' }]],
-        syntaxHighlight: 'shiki',
+        rehypePlugins: [[externalLink, { domain: "fparedes.com" }]],
+        syntaxHighlight: "shiki",
         shikiConfig: {
-            theme: 'dracula-soft',
-        },
+            theme: "dracula-soft"
+        }
     },
     redirects: {
-        '/wczgz2020': '/talks/2020/limpia-tu-codigo',
-    },
-})
+        "/wczgz2020": "/talks/2020/limpia-tu-codigo"
+    }
+});
