524c1f960cba0ea44bfeeb96827d67daf4f58845
fernando-paredes <ferparmur@gmail.com>
2024-08-10 04:39:45 +0100

    made typescript happy

--- src/data/schema/breadcrumbList.ts
+++ src/data/schema/breadcrumbList.ts
@@ -1,5 +1,24 @@
-const breadcrumbList = (canonicalUrl: string, items: Array<any>) => {
-    const list = {
+type BreadcrumbItem = {
+    name: string;
+    item?: string;
+};
+
+type BreadcrumbList = {
+    "@type": "BreadcrumbList";
+    "@id": string;
+    itemListElement: Array<{
+        "@type": "ListItem";
+        position: number;
+        name: string;
+        item?: string;
+    }>;
+};
+
+const breadcrumbList = (
+    canonicalUrl: string,
+    items: Array<BreadcrumbItem>,
+): BreadcrumbList => {
+    const list: BreadcrumbList = {
         "@type": "BreadcrumbList",
         "@id": `${canonicalUrl}#breadcrumb`,
         itemListElement: [],
@@ -7,14 +26,11 @@ const breadcrumbList = (canonicalUrl: string, items: Array<any>) => {
 
     for (let i = 0; i < items.length; i++) {
         const itemListElement = {
-            "@type": "ListItem",
+            "@type": "ListItem" as const, // Explicitly cast to the string literal type
             position: i + 1,
             name: items[i].name,
+            item: items[i].item,
         };
-
-        if (items[i].item) {
-            itemListElement["item"] = items[i].item;
-        }
 
         list.itemListElement.push(itemListElement);
     }
